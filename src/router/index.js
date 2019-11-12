32cb3e896a225604051731ce4bedb38f6c9af6c8
WEB-brgAO <1712030121@qq.com>
2019-11-12 23:34:01 +0800

    黑马头条PC-初始化项目

--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,22 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+  path: '/',
+  name: 'home',
+  component: Home
+}
+  // 按需加载
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   // 箭头函数是 按需加载了意思
+  //   component: () =>
+  //       import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
