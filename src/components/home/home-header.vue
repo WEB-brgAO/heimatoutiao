0179097f17e1495a53747bd6ee084f5fe5a8fac6
WEB-brgAO <1712030121@qq.com>
2019-11-14 17:05:28 +0800

    黑马头条PC-主页模块-头部结构及下拉菜单

---
+++ src/components/home/home-header.vue
@@ -0,0 +1,60 @@
+<template>
+ <el-row type="flex" class="min">
+   <el-col class="left" :span="12">
+    <i class="el-icon-s-fold"></i>
+    <span>江苏传智播客教育科技股份有限公司</span>
+   </el-col>
+   <el-col class="right" :span="3">
+     <img src="../../assets/img/avatar.jpg" alt="">
+     <el-dropdown trigger="click">
+       <span class="el-dropdown-link">
+        用户名<i class="el-icon-arrow-down el-icon--right"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item >黄金糕</el-dropdown-item>
+          <el-dropdown-item >狮子头</el-dropdown-item>
+          <el-dropdown-item >螺蛳粉</el-dropdown-item>
+          <el-dropdown-item >双皮奶</el-dropdown-item>
+          <el-dropdown-item >蚵仔煎</el-dropdown-item>
+      </el-dropdown-menu>
+      </span>
+     </el-dropdown>
+   </el-col>
+ </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  .min {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0;
+    .left {
+      display: flex;
+      align-items: center;
+      i {
+        font-size: 30px;
+      }
+      span {
+        margin-left: 6px;
+      }
+    }
+    .right {
+      vertical-align: middle;
+      display: flex;
+      align-items: center;
+      img {
+        width: 40px;
+        border-radius: 25px;
+      }
+      span {
+        margin-left: 20px;
+      }
+
+    }
+  }
+</style>
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,13 +1,66 @@
 <template>
-  <div>主页</div>
+  <div>
+    <!-- 外部容器 -->
+    <el-container>
+      <!-- 左侧边栏 -->
+      <el-aside class="left" style="width:180px;background-color:red" >
+        <div>
+          <img src="../../assets/img/logo_admin.png" alt="">
+        </div>
+        <el-menu style="width:181px" background-color="#353b4e" active-text-color="#ffd04b" text-color=" #adafb5" >
+          <el-menu-item>首页</el-menu-item>
+          <el-submenu>
+            <template slot="title">内容管理</template>
+            <el-menu-item>发布文章</el-menu-item>
+            <el-menu-item>内容列表</el-menu-item>
+            <el-menu-item>评论列表</el-menu-item>
+            <el-menu-item>素材管理</el-menu-item>
+          </el-submenu>
+          <el-submenu>
+            <template slot="title">粉丝管理</template>
+            <el-menu-item>图文数据</el-menu-item>
+            <el-menu-item>粉丝概况</el-menu-item>
+            <el-menu-item>粉丝画像</el-menu-item>
+            <el-menu-item>粉丝列表</el-menu-item>
+          </el-submenu>
+          <el-menu-item>账户信息</el-menu-item>
+        </el-menu>
+      </el-aside>
+      <!-- 右侧 -->
+      <el-container>
+        <el-header>
+          <!-- 头部 -->
+          <ls-header></ls-header>
+        </el-header>
+        <el-main>
+          <!-- 二级容器 -->
+          容器
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script>
+import headers from './home-header.vue'
 export default {
-
+  path: 'home',
+  components: {
+    'ls-header': headers
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+  .left {
+    overflow: hidden;
+    background-color:#353b4e ;
+    div {
+      text-align: center;
+      padding: 10px 0;
+      img {
+        width: 120px;
+      }
+    }
+  }
 </style>
